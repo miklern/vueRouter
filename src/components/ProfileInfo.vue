f1392de2760277c31ccfd2493f9a6df5d7488eef
Mikhail Baklan <mi.bak@mail.ru>
2021-07-07 22:40:28 +0300

    remove iteration

--- src/components/ProfileInfo.vue
+++ src/components/ProfileInfo.vue
@@ -2,40 +2,38 @@
   <div class="profile-info">
     <div class="user-ifno">
       <div
-        v-for="userInformation in usersInformation"
-        :key="userInformation.id"
         class="user-ifno__block"
       >
         <div class="user-info__title">
-          Info about: {{ userInformation.name }}
+          Info about: {{ usersInformation.name }}
         </div>
         <div class="user-info__item">
           <span>Username:</span>
-          {{ userInformation.username }}
+          {{ usersInformation.username }}
         </div>
         <div class="user-info__item">
           <span>Email:</span>
-          {{ userInformation.email }}
+          {{ usersInformation.email }}
         </div>
         <div class="user-info__item">
           <span>Street:</span>
-          {{ userInformation.address.street }}
+          {{ usersInformation.address.street }}
         </div>
         <div class="user-info__item">
           <span>Suite:</span>
-          {{ userInformation.address.suite }}
+          {{ usersInformation.address.suite }}
         </div>
         <div class="user-info__item">
           <span>City:</span>
-          {{ userInformation.address.city }}
+          {{ usersInformation.address.city }}
         </div>
         <div class="user-info__item">
           <span>Phone:</span>
-          {{ userInformation.phone }}
+          {{ usersInformation.phone }}
         </div>
         <div class="user-info__item">
           <span>Website:</span>
-          {{ userInformation.website }}
+          {{ usersInformation.website }}
         </div>
       </div>
     </div>
@@ -69,23 +67,23 @@ export default {
   },
   data() {
     return {
-      usersInformation: [],
+      usersInformation: {},
       userAlbums: [],
     };
   },
   async created() {
-    await this.getUsersList();
+    await this.getUserInfo();
     await this.getUserAlbum();
   },
   methods: {
-    async getUsersList() {
+    async getUserInfo() {
       const filters = {};
       if (this.userId !== null) {
         filters.id = this.userId;
       }
       const usersInformation = await fetchOneUser(filters);
       if (usersInformation) {
-        this.usersInformation = usersInformation;
+        this.usersInformation = usersInformation.shift();    
       }
     },
     async getUserAlbum() {
@@ -102,7 +100,7 @@ export default {
   watch: {
     async userId(newValue, oldValue) {
       if (newValue !== oldValue) {
-        await this.getUsersList();
+        await this.getUserInfo();
         await this.getUserAlbum();
       }
     },
