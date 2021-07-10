35053e461cc490ebe07d7dc959efb909b050370e
Mikhail Baklan <mi.bak@mail.ru>
2021-07-10 15:03:51 +0300

    rename const

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -2,7 +2,7 @@
   <div class="users-list">
     <div class="users-list__body">
       <div
-        v-for="user in userInfo"
+        v-for="user in usersInfo"
         :key="user.id"
         @click="$emit('onUserSelect', user.id), (activeName = user.id)"
         :class="{ active: activeName === user.id }"
@@ -20,14 +20,14 @@ export default {
   name: "UsersList",
   data() {
     return {
-      userInfo: [],
+      usersInfo: [],
       activeName: null,
     };
   },
   async created() {
-    const userInfo = await fetchUsersInfo();
-    if (userInfo) {
-      this.userInfo = userInfo;
+    const usersInfo = await fetchUsersInfo();
+    if (usersInfo) {
+      this.usersInfo = usersInfo;
     }
   },
 };
